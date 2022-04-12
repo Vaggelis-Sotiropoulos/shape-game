03552cb334b745ba5e804daf19f85b4948e77120
Vaggelis Sotiropoulos <vaggelissotiropoulos@Vaggeliss-MacBook-Pro.local>
2022-04-12 14:31:23 -0700

    feat(shape-game): React shell

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+/** Potential polyfills
+import 'core-js/features/array/flat-map'
+import 'core-js/features/map'
+import 'core-js/features/promise'
+import 'core-js/features/set'
+import 'raf/polyfill'
+import 'whatwg-fetch'
+*/
+
+import './index.css'
+
+ReactDOM.render(
+    <h1>Welcome to Shape Game!</h1>,
+    document.getElementById('app-root'),
+)
